722da0b5fe8b2e98230d35e432231fc06845d038
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-18 16:52:15 +0300

    array-find-method-optional lab DONE

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
 // code your solution here
+// let finder = record.find((record,superbowlWin)=>{
+//     let myResult = null
+//     for (const item of record) {
+//         if (superbowlWin(item) === true){
+//             myResult = item.year
+
+//         }
+        
+//     }
+//     return myResult
+// });
+// function superbowlWin(item) {
+//     return item.result === "W";
+//   }
+
+
+function  superbowlWin(record){
+    for (const item of record) {
+        if (item.result === "W"){
+            return item.year
+        }
+        
+    }
+}
+let finder = record.find(superbowlWin)
+console.log(finder);
